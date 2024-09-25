7893aa8026c04650f3b54fe7f7955dcc24223a9f
vbalalian <vbalalian@gmail.com>
2024-09-25 15:45:07 -0700

    initial commit

---
+++ scraper_all.py
@@ -0,0 +1,145 @@
+import os
+import mechanize
+from google.cloud import bigquery
+from bs4 import BeautifulSoup
+import http.cookiejar as cookielib
+import pandas as pd
+import dataframe_image as dfi
+import requests
+
+# Littlefield website info
+class_url = os.getenv('CLASS_URL')
+group_id = os.getenv('GROUP_ID')
+group_pw = os.getenv('GROUP_PW')
+
+# Google Cloud project info
+project_id = os.getenv('PROJECT_ID')
+dataset_name = os.getenv('DATASET_NAME')
+bigquery_table = os.getenv('TABLE_NAME')
+gcs_bucket = os.getenv('GCS_BUCKET')
+
+# Discord webhook
+webhook = os.getenv('DISCORD_WEBHOOK')
+
+def Browser() -> mechanize.Browser:
+    '''Returns mechanize.Browser object with set cookiejar'''
+    cj = cookielib.CookieJar()
+    br = mechanize.Browser()
+    br.set_cookiejar(cj)
+    return br
+
+def login(url:str, id:str, pw:str, browser:mechanize.Browser):
+    '''Logs into Littlefield simulation URL'''
+    print('Logging in...')
+    browser.open(url)
+    browser.select_form(nr=0)
+    browser.form['id'] = id
+    browser.form['password'] = pw
+    browser.submit()
+
+def scrape_data(browser:mechanize.Browser) -> pd.DataFrame:
+    '''Scrapes data from Littlefield categorical URLS, returns it as a DataFrame'''
+    # Set variables
+    url_list = ["INV", "AVGINV", "CASH","JOBIN", "JOBREJECTS", "JOBQ", "JOBWIP", "S1Q","S2Q","S3Q","S1UTIL","S2UTIL","S3UTIL"]
+    url_list_4col = ["JOBT","JOBREV","JOBOUT"]
+    LF_DATA = {}
+
+    # Scrape url_list
+    for url in url_list:
+        print(f'Scraping {url}')
+        lf_url = "http://op.responsive.net/Littlefield/Plot?data=%s&x=all" % url
+        soup = BeautifulSoup(browser.open(lf_url), "lxml")
+        data = soup.find_all("script")[6].string
+        data = data.split("\n")[4].split("'")[3].split()
+        values = []
+        for i in range(len(data)):
+            if i % 2 == 0 and "." not in data[i]:
+                values.append(data[i+1])
+        LF_DATA[url] = values
+
+    # Scrape url_list_4col
+    for url in url_list_4col:
+        print(f'Scraping {url}')
+        lf_url = "http://op.responsive.net/Littlefield/Plot?data=%s&x=all" % url
+        soup = BeautifulSoup(browser.open(lf_url), "lxml")
+        data = soup.find_all("script")[6].string
+        for i in range(3):
+            try:
+                data = data.split("\n")[i+4].split("'")[5].split()
+                values = []
+                for j in range(len(data)):
+                    if j % 2 == 0 and "." not in data[j]:
+                        values.append(data[j+1])
+                LF_DATA[url+str(i+1)] = values
+            except:
+                # Fill Contract 2 and 3 columns with 0s
+                values = [0 for _ in range(len(LF_DATA['INV']))]
+                LF_DATA[url+str(i+1)] = values
+                continue
+
+    df = pd.DataFrame(LF_DATA, dtype=float)
+    df.index += 1
+    df.index.name = 'DAY'
+    return df
+
+def save_to_bucket(df:pd.DataFrame, bucket:str, filename:str):
+    '''Saves DataFrame to csv in GCS bucket'''
+    print(f'Saving CSV file {filename} to bucket {bucket}...')
+    df.to_csv(f'gs://{bucket}/{filename}')
+    print('...complete.')
+
+def load_to_bigquery(df:pd.DataFrame, project:str, dataset:str, table:str):
+    '''Loads DataFrame into BigQuery table'''
+    table_id = f'{project}.{dataset}.{table}'
+    print(f'Loading to BigQuery table:{table_id}')
+    client = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        write_disposition = "WRITE_TRUNCATE"
+    )
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    print('...complete.')
+    return job.result()
+
+def daily_report(df:pd.DataFrame, team_name:str) -> pd.DataFrame:
+    '''Transforms DataFrame into eye friendly report based on values dict'''
+    present_day = df.index.max()
+    report_df = pd.DataFrame({
+        'Day':[present_day], 
+        'Cash':[df.loc[present_day, 'CASH']],
+        'Inventory':[df.loc[present_day, 'INV']],
+        'Job Demand':[df.loc[present_day, 'JOBIN']],
+        'Avg Demand (5 day)':[df.loc[present_day-5:present_day, 'JOBIN'].mean()],
+        'Station 1 Util (%)':[df.loc[present_day, 'S1UTIL']*100],
+        'St 1 Avg(5) Util (%)':[df.loc[present_day-5:present_day, 'S1UTIL'].mean()*100],
+        'Station 2 Util (%)':[df.loc[present_day, 'S2UTIL']*100],
+        'St 2 Avg(5) Util (%)':[df.loc[present_day-5:present_day, 'S2UTIL'].mean()*100],
+        'Station 3 Util (%)':[df.loc[present_day, 'S3UTIL']*100],
+        'St 3 Avg(5) Util (%)':[df.loc[present_day-5:present_day, 'S3UTIL'].mean()*100],
+        }, index=[team_name]).transpose()
+    report_df = report_df.style.format(precision=0)
+    return report_df
+
+def post_report_to_discord(df:pd.DataFrame, discord_webhook:str, filename:str='chart.png', font_size:int=10) -> requests.Response:
+    '''Posts image to Discord webhook, returns requests.Response'''
+    print('Posting report image to Discord...')
+    temp_file = '/tmp/' + filename
+    dfi.export(df, filename=temp_file, table_conversion='matplotlib', fontsize=font_size)
+    with open(temp_file, 'rb') as image:
+        response = requests.post(discord_webhook,
+                files={filename: image})
+    print('...complete.')
+    return response
+
+def main(request):
+    browser = Browser()
+    login(url=class_url, id=group_id, pw=group_pw, browser=browser)
+    data = scrape_data(browser=browser)
+    save_to_bucket(df=data, bucket=gcs_bucket, filename='data.csv')
+    load_to_bigquery(
+        df=data, project=project_id, dataset=dataset_name, table=bigquery_table
+        )
+    post_report_to_discord(daily_report(df=data, team_name=group_id), discord_webhook=webhook)
+    return 'Success', 200
+
+if __name__ == '__main__':
+    main()
